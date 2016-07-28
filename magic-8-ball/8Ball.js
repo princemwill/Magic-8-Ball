d771728bd825c5828f0a8c861e736559f2dad7b3
Prince <princemwilliams@gmail.com>
2016-07-28 10:27:01 -0400

    Foundation of 8 ball. Button and basic list of responses generated. Also have  barebones html.

---
+++ magic-8-ball/8Ball.js
@@ -0,0 +1,22 @@
+﻿
+
+
+function fortune(){
+    //randomly pick a phrase
+    //and display it
+    var answer = document.getElementById('answer')
+       
+
+    var randomAnswers = [
+        'Maybe, but is it worth it?',
+        'The answer is within you. Be still and listen.',
+        "Want is the source of all suffering!",
+        "You mad or nah?..",
+        "Uhhhhhh.....nah!",
+        'Yes! Now share the wealth!',
+        "Maybe, but with great power, comes great responsibilty!"];
+
+
+    answer.innerHTML = randomAnswers[2];
+    
+}
